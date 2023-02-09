9044223c18c3e0d896f541c0c061225f18336e5f
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-09 16:13:36 -0300

    Atualização

---
+++ Cap_1/hello_word.py
@@ -0,0 +1,6 @@
+#Bruno David 09/02/2023 
+#Esse codigo faz com que o script seja executado apenas uma vez.
+#E faz com que apareça uma menssagem "hello word"
+
+message = "hello word!"
+print (message)
---
+++ Cap_2/motorcycles.py
@@ -0,0 +1,3 @@
+motorcycle = ['honda', 'yamaha', 'suzuki']
+motorcycle[0] = 'ducati' 
+print(motorcycle)
