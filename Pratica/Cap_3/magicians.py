884575c1fdc1252d7a3a534fe131691924f3ca80
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-10 18:05:29 -0300

    atualização

---
+++ Pratica/Cap_3/magicians.py
@@ -0,0 +1,6 @@
+magicians = ['alice', 'david', 'carolina']
+for magicians in magicians:
+    print(magicians.title() + ' that was a great trick.')
+    print("i can't to wait to see your next trick. " + magicians.title() + ".\n")
+
+print('thank you, everyone. that was a great magic show!')
