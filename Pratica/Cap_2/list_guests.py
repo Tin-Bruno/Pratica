dbcf1e6e3bad9c2221969fe6429e44ea1f27065a
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-09 21:20:39 -0300

    atz

---
+++ Pratica/Cap_2/list_guests.py
@@ -0,0 +1,19 @@
+invite=['fag','lucas','bruno']
+
+invite.insert(0,'gabriel')
+invite.insert(1,'michael')
+invite.append('saulo')
+
+invite.pop(0)
+invite.pop(-1)
+invite.pop(-2)
+invite.pop(-3)
+    
+print ('olá, ' + (invite[0]) + ' wonld  would to go a dinner in weekend')
+print ('olá, ' + (invite[1]) + ' wonld  would to go a dinner in weekend')
+
+print ("kaio can't going a dinner in weekend")
+
+print ("I got a bigger table")
+
+print ("i can to invite only two persons")
