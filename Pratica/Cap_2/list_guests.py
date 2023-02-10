7671706d3fc17688ccef00898f919a4bae305a22
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-10 11:58:12 -0300

    atualização

--- Pratica/Cap_2/list_guests.py
+++ Pratica/Cap_2/list_guests.py
@@ -4,16 +4,20 @@ invite.insert(0,'gabriel')
 invite.insert(1,'michael')
 invite.append('saulo')
 
-invite.pop(0)
-invite.pop(-1)
-invite.pop(-2)
-invite.pop(-3)
+invite.pop()
+invite.pop()
+invite.pop()
+invite.pop()
+
+invite_len = len(invite)
     
 print ('olá, ' + (invite[0]) + ' wonld  would to go a dinner in weekend')
 print ('olá, ' + (invite[1]) + ' wonld  would to go a dinner in weekend')
 
-print ("kaio can't going a dinner in weekend")
+print ("\nkaio can't going a dinner in weekend")
 
-print ("I got a bigger table")
+print ("\nI got a bigger table")
 
-print ("i can to invite only two persons")
+print ("\ni can to invite only two persons")
+
+print ("\ninvited persons: "+ str(invite_len))
---
+++ Pratica/Cap_2/lits_video_games.py
@@ -0,0 +1,6 @@
+video_games = ['ps4','xbox x','nintendo']
+video_games.sort()
+video_games.reverse()
+video_games_len = len(video_games)
+print('video games i wish i had ' + str(video_games))
+print('\nnumber to video games: ' + str(video_games_len))
