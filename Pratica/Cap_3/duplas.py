c4d9dfcbb24e66502a7881bd500b86630f3c8d61
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-13 13:44:24 -0300

    atualizção

---
+++ Pratica/Cap_3/duplas.py
@@ -0,0 +1,9 @@
+buffet = ('bife','tomate','salada','frango','arroz') 
+print('\noriginal Cardapio: ')
+for food in buffet:
+    print(food)
+
+buffet = ('bife','repolho','salada','peixe','arroz')
+print('\nCardapio modified: ')
+for food in buffet:
+    print (food)
