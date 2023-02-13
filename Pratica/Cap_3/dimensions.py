b926e82e7c28afd038d18f05421852cd265659ec
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-13 13:43:57 -0300

    atualizção

--- Pratica/Cap_3/dimensions.py
+++ Pratica/Cap_3/dimensions.py
@@ -1,1 +1,9 @@
-dimension = (200,50)
+dimensions = (200,50)
+print ("original dimensions: ",)
+for dimension in dimensions:
+    print (dimension)
+
+dimensions = (400,100)
+print ("\nmodified dimensions: ",)
+for dimension in dimensions:
+    print (dimension)
