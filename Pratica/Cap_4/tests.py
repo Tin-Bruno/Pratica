16e2dec0826df8c02fb3bd4d791d8c3a98689e78
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-15 13:56:54 -0300

    atualização

--- Pratica/Cap_4/tests.py
+++ Pratica/Cap_4/tests.py
@@ -1,1 +1,23 @@
-car = 'bmw'
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+book = 'curso python'
+print("\nIs book == 'curso python'? I predict True.")
+print(book == 'curso python')
+print("\nIs book == 'curso java'? I predict False.")
+print(book == 'curso java')
+
+computer = 'laptop'
+print("\nIs computer == 'laptop'? I predict True.")
+print(computer == 'laptop')
+print("\nIs computer == 'pc'? I predict False.")
+print(computer == 'pc')
+
+cell = 'celular'
+print("\nIs cell == 'celular'? I predict True.")
+print(cell == 'celular')
+print("\nIs cell == 'cel'? I predict False.")
+print(cell == 'cel')
