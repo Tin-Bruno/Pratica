5933dc9387c6ce77c7ecf1fc7bc8f8a5e85ba83f
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-17 15:39:12 -0300

    atualização

--- Pratica/Cap_4/tests.py
+++ Pratica/Cap_4/tests.py
@@ -1,23 +1,28 @@
 car = 'subaru'
 print("Is car == 'subaru'? I predict True.")
-print(car == 'subaru')
+print(car
+      == 'subaru')
 print("\nIs car == 'audi'? I predict False.")
-print(car == 'audi')
-
+print(car
+      == 'audi')
 book = 'curso python'
 print("\nIs book == 'curso python'? I predict True.")
-print(book == 'curso python')
+print(book
+      == 'curso python')
 print("\nIs book == 'curso java'? I predict False.")
-print(book == 'curso java')
-
+print(book
+      == 'curso java')
 computer = 'laptop'
 print("\nIs computer == 'laptop'? I predict True.")
-print(computer == 'laptop')
+print(computer
+      == 'laptop')
 print("\nIs computer == 'pc'? I predict False.")
-print(computer == 'pc')
-
+print(computer 
+      == 'pc')
 cell = 'celular'
 print("\nIs cell == 'celular'? I predict True.")
-print(cell == 'celular')
+print(cell 
+      == 'celular')
 print("\nIs cell == 'cel'? I predict False.")
-print(cell == 'cel')
+print(cell
+      == 'cel')
--- Pratica/Cap_4/topping_1.py
+++ Pratica/Cap_4/topping_1.py
@@ -1,4 +1,5 @@
-requested_topping = ['mushrooms', 'extra cheese']
+requested_topping = ['mushrooms',
+                     'green pepper',]
 if 'mushrooms' in requested_topping:
     print('Adding  mushrooms')
 if 'peperone' in requested_topping:
