189c84bd57ed22966ae1ddbf773b0efcf56fddfe
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-18 20:05:45 -0300

    atualização

--- Pratica/cap_5/glossario.py
+++ Pratica/cap_5/glossario.py
@@ -1,10 +1,7 @@
-glossario = {'title': 'deixa a primeira letra maiúscula',
-             'str': 'torna o valor da string literal',
-             'if': 'se o valor da condição for verdadeiro',
-             'elif': 'é executado se caso if for falso',
-             'else': ' normalmente executado após alguma string'}
-for significado in glossario:
-    print('\nSignificado de: '
-          + significado.title()
-          + ': '
-          + glossario[significado])
+glossario = {'Title': 'deixa a primeira letra maiúscula',
+             'Str': 'torna o valor da string literal',
+             'If': 'se o valor da condição for verdadeiro',
+             'Elif': 'é executado se caso if for falso',
+             'Else': 'normalmente executado após alguma string'}
+for significado in glossario.items():
+    print("\nSignificado: ", significado)
