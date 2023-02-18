e5f59862f49dc4fc32cf674452cee5e8b7a10619
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-18 12:49:23 -0300

    atualização

---
+++ Pratica/Cap_4/hello_admin.py
@@ -0,0 +1,6 @@
+AcountGrupe = ["bruno", "carlos", "daniel", "adimin", "lucas",]
+for acount in AcountGrupe:
+    if acount == "adimin":
+        print("olá adimin, would like a report?")
+    else:
+        print("olá " + acount + ", thanks for to make login again.")
