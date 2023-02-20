8c6c78adfe85a2981c99c92ebcc2610894048158
TinDum <brunodavidmoreirabh2@gmail.com>
2023-02-20 11:40:47 -0300

    atualização

--- Pratica/cap_5/favorite_language.py
+++
@@ -1,5 +0,0 @@
-favorite_languages = {'jen': 'python',
-                      'sarah': 'c',
-                      'edward': 'ruby',
-                      'phil': 'python', }
-print("Sarah's favorite language is " + favorite_languages['sarah'] + ".")
