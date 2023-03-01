bb54a4e326fbfddc188da6e27e9cb6e3f6a5db51
Bruno David M. De A. Rodrigues <53412733+Tin-Bruno@users.noreply.github.com>
2023-03-01 12:49:21 -0300

    Update sandwich_orders.py

--- Pratica/Cap_6/sandwich_orders.py
+++ Pratica/Cap_6/sandwich_orders.py
@@ -3,7 +3,9 @@ finally_sandwiches = []
 while sandwich_orders:
     making_sandwich = sandwich_orders.pop()
     print("Sandwich makeing: ", making_sandwich.title())
+
     finally_sandwiches.append(making_sandwich)
     print("\nFinally sandwiches:")
+
     for finally_sandwich in finally_sandwiches:
         print(finally_sandwich.title() + " to be served")
