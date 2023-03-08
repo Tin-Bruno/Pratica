59f7defed2dc1e2e063831490981d0a8fe91b63b
TinDum <brunodavidmoreirabh2@gmail.com>
2023-03-08 13:19:02 -0300

    atualização

--- Pratica/Cap_7/pets.py
+++ Pratica/Cap_7/pets.py
@@ -1,6 +1,7 @@
-def describre_pets(animal_type, pet_name):
+def describre_pets(pet_name, animal_type='dog'):
     print("\nI'have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name + ".")
 
 
-describre_pets('hamister', 'harry')
+describre_pets('willie')
+describre_pets(pet_name='harry', animal_type='hamster')
