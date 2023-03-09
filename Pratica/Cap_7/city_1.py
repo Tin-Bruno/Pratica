dee524efbf7abe4372d6b305e63c01f9d2ed24d7
TinDum <brunodavidmoreirabh2@gmail.com>
2023-03-09 16:21:00 -0300

    atualização

--- Pratica/Cap_7/city_1.py
+++
@@ -1,8 +0,0 @@
-def describe_city(name_city, location_city='Brasil'):
-    print('\nThe name of the city is: ' + name_city)
-    print('The location of the city is: ' + location_city)
-
-
-describe_city('São Paulo')
-describe_city('Rio de Janeiro')
-describe_city(name_city='New York', location_city='Estados Unidos')
