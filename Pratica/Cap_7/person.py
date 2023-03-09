a2e55e9364d7d2022dc11b909adf4ee9ae2dea7c
TinDum <brunodavidmoreirabh2@gmail.com>
2023-03-09 14:49:48 -0300

    atualização

--- Pratica/Cap_7/person.py
+++ Pratica/Cap_7/person.py
@@ -1,7 +1,9 @@
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age=''):
     person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = int(age)
     return person
 
 
-musician = build_person('jimi', 'hendrix')
+musician = build_person('jimi', 'hendrix', age=str(27))
 print(musician)
